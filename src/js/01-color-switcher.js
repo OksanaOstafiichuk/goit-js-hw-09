b32fdcbfefe2a94c09f30f2e0353aacd9f90ae38
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-05-17 21:12:49 +0300

    Upedate 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+}
+
+let timerId = null;
+
+refs.start.addEventListener('click', onClickStart);
+refs.stop.addEventListener('click', onClickStop);
+
+function onClickStart(evt) {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+
+    refs.start.disabled = true;
+    refs.stop.disabled = false;
+}
+
+function onClickStop(evt) {
+    clearInterval(timerId);
+    refs.start.disabled = false;
+    refs.stop.disabled = true;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
